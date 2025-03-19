27cb4679a60dfa08aff35a1c749d551d4bb35f1c
Dirk Jäger <dirk.jaeger@schufa.de>
2025-03-19 19:03:57 +0100

    fix builds

--- build/_build-and-deploy-to-artifactory.sh
+++ build/_build-and-deploy-to-artifactory.sh
@@ -1,49 +1,69 @@
 #!/bin/bash
 set -eo pipefail
 
+if [ -z "$1" ]; then
+    echo "No build is set" 1>&2
+    exit 1
+fi
 
+BINARY_NAME="$1"
+BINARY_NAME_UBI7="${BINARY_NAME}-ubi7"
+IMAGE="${BINARY_NAME}:ubi7"
+CONTAINER_NAME="${BINARY_NAME}-container"
 
-# build the linux binary of kpasscli
-echo "Build linux binary of kpasscli"
-go build -v
+mkdir -p dist
+go mod tidy
+# build the linux binary
+echo "Build linux binary of $BINARY_NAME"
+go build -v -o dist
 
-# build the windows binary of kpasscli
-echo "Build windows binary of kpasscli"
-GOOS=windows GOARCH=amd64 go build -v
+# build the windows binary
+echo "Build windows binary of $BINARY_NAME"
+GOOS=windows GOARCH=amd64 go build -v -o dist
 
-# check kpasscli and if it works, then upload to artifactory
-if ./kpasscli -c config.yaml -i pw1.1 -f username; then
+# check binary and if it works, then upload to artifactory
+if dist/$BINARY_NAME -h >/dev/null && go test; then
     echo "Push to artifactory"
 
-    artifactory-upload.sh -lf=kpasscli       -tr=scptools-bin-dev-local   -tf=/tools/kpasscli/kpasscli-linux/
-    artifactory-upload.sh -lf=kpasscli       -tr=scpas-bin-dev-local      -tf=/istag_and_image_management/kpasscli-linux/
+    artifactory-upload.sh -lf=dist/$BINARY_NAME       -tr=scptools-bin-dev-local     -tf="tools/$BINARY_NAME"
+    artifactory-upload.sh -lf=dist/$BINARY_NAME       -tr=scptools-bin-dev-local     -tf="ocp-stable-4.16/clients/$BINARY_NAME"
 
-    artifactory-upload.sh -lf=kpasscli.exe   -tr=scptools-bin-dev-local   -tf=/tools/kpassclis/kpasscli-windows/
-    artifactory-upload.sh -lf=kpasscli.exe   -tr=scpas-bin-dev-local      -tf=/istag_and_image_management/kpasscli-windows/
+    artifactory-upload.sh -lf=dist/$BINARY_NAME.exe   -tr=scptools-bin-dev-local     -tf="tools/$BINARY_NAME"
+    artifactory-upload.sh -lf=dist/$BINARY_NAME.exe   -tr=scptools-bin-dev-local     -tf="ocp-stable-4.16/clients/$BINARY_NAME"
 
     echo "Copy it to share folder PEWI4124://Daten"
-    cp kpasscli kpasscli.exe  /gast-drive-d/Daten/
+    cp dist/$BINARY_NAME dist/$BINARY_NAME.exe  /gast-drive-d/Daten/
 fi
 
 echo
 echo
 echo "#  B U I L D   I M A G E   T O O L   F O R   U B I 7"
-BINARY_NAME="kpasscli"
-BINARY_NAME_UBI7="dist/${BINARY_NAME}-ubi7"
-IMAGE="${BINARY_NAME}:ubi7"
-CONTAINER_NAME="${BINARY_NAME}-container"
+echo "bin in $PWD"
 
 # build ubi7 binary in image
-/usr/bin/podman build -t "$IMAGE" -f Dockerfile .
+/usr/bin/podman build -t "$IMAGE" -f Dockerfile-ubi7
 
 echo "##########  copy binary from container to local  ##########"
 if podman ps -a | rg "$CONTAINER_NAME" >/dev/null; then
     podman rm "$CONTAINER_NAME"
 fi
 podman create --name "$CONTAINER_NAME" "localhost/$IMAGE"
-podman cp "$CONTAINER_NAME":/app/dist/kpasscli "$BINARY_NAME_UBI7"
-scp "$BINARY_NAME_UBI7" cid-scp0-tls-v01-mgmt:
+podman cp "$CONTAINER_NAME":/app/dist/$BINARY_NAME "dist/$BINARY_NAME_UBI7"
 podman rm "$CONTAINER_NAME"
 
-artifactory-upload.sh -lf="$BINARY_NAME_UBI7" -tr=scptools-bin-dev-local  -tf=ocp-stable-4.16/clients/$BINARY_NAME
+artifactory-upload.sh -lf="dist/$BINARY_NAME_UBI7"   -tr=scptools-bin-dev-local   -tf="tools/$BINARY_NAME"
+artifactory-upload.sh -lf="dist/$BINARY_NAME_UBI7"   -tr=scptools-bin-dev-local   -tf="ocp-stable-4.16/clients/$BINARY_NAME"
 
+for cl in dev-scp0 cid-scp0 ppr-scp0 vpt-scp0 pro-scp0 pro-scp1; do
+    nodepattern=tls-v01-mgmt
+    desthost=$cl-$nodepattern
+    if [[ $cl =~ dev-scp1-c[12] ]]; then
+        clnr=$(echo $cl | cut -d'-' -f3)
+        nodepattern=${clnr}t-v01-mgmt
+        desthost=$cl$nodepattern
+    fi
+    if scp dist/$BINARY_NAME_UBI7 $desthost:/tmp/; then
+        echo ansible-shell ocp $cl $nodepattern "install /tmp/$BINARY_NAME_UBI7 /usr/local/bin/$BINARY_NAME"
+        ansible-shell ocp $cl $nodepattern "install -v /tmp/$BINARY_NAME_UBI7 /usr/local/bin/$BINARY_NAME"
+    fi
+done
--- build/build-and-deploy.sh
+++ build/build-and-deploy.sh
@@ -1,35 +1,29 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
+. ocl cid-scp0 >/dev/null 2>&1
+
 script="$(basename "$0")"
 scriptdir="$(dirname "$0")"
 dir=$(dirname "$scriptdir")
 echo "script: $script, scriptdir: $scriptdir, Dir: $dir"
 cd "$dir"
-quayurl="registry-quay-quay.apps.pro-scp1.sf-rz.de"
-if podman login -u "$USER" -p "$(eval "$LDAPPASSWORDPROVIDER")" "$quayurl"; then
+
+# set this variable to correct values
+CLUSTER=cid-scp0
+quayurl="registry-quay-quay.apps.pro-scp1.sf-rz.de"     # default='registry-quay-quay.apps.pro-scp1.sf-rz.de'
+build='kpasscli'                               # name of the go binary
+build_service_image='false'            # name of the image-stream, if a service image should be created, default='false'. If not false, you need a Dockerfile in repo root.
+tagversion="latest"
+# end var
+
+if podman login -u "$USER" -p "$(eval "$LDAPPASSWORDPROVIDER")" "$quayurl" >/dev/null 2>&1; then
     echo "quayurl: $quayurl"
 else
     echo "ERROR: podman login failed" 1>&2
     exit 1
 fi
 
-# Set defaults and evaluate commandline parameters
-CLUSTER=cid-scp0
-optspec=":bpidvhct-:"
-tagversion="$(get-git-tag.sh)"
-# per default make it all
-build='true'
-preparebuild='true'
-imagebuild='true'
-deploy='true'
-# if one or more params is given, then only this params is executed
-if [ $# -gt 0 ]; then
-    build='false'
-    preparebuild='false'
-    imagebuild='false'
-    deploy='false'
-fi
 
 hilfe() {
     if [[ -n $1 ]]; then
@@ -43,95 +37,99 @@ hilfe() {
         $script [-b|--build] [-h|--help]
 
     OPTIONS
-        -b | --build
-            Enable to build the kpasscli executable and deploy to artifactory
+        -b <name> | --build[=]<name>
+            Enable to build the $build executable and deploy to artifactory. Default is '$build'
+        -t <tag> | --tag[=]<tag>
+            The tag version of the $build to build. Default is 'latest' tag, thats means the head in git repo.
+            With '-t newest | --tag newest' the newest tag in git is used.
+            If the tag is not lattest (default), the the git repo is checked out to the given tag for the build.
         -h | --help
             Print this help message
 
     DESCRIPTION
-        $script builds and deploys the kpasscli to the specified cluster.
+        $script builds and deploys the $build to the specified cluster.
 
     EXAMPLES
-        $script -b -t v1.0.0
-            Build the kpasscli with tag v1.0.0 and deploy it
-        $script -b
-            Build the kpasscli with the latest tag
-
+        $script [-b name] -t v1.0.0
+            Build the $build with tag v1.0.0 and deploy it
+        $script [-b name] -t newest
+            Build the $build with the newest tag that is defined in git
+        $script [-b name]
+            Build the $build with the latest tag
 EOH
 }
 
-
-while getopts "$optspec" optchar; do
-    case "${optchar}" in
-        -)  # Evaluate long options
-            case "${OPTARG}" in
-                build)
-                    val="${!OPTIND}";
-                    echo "Parsing option: '--${OPTARG}', value: '${val}'" >&2;
-                    build='true'
-                    ;;
-                tag)
-                    val="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
-                    echo "Parsing option: '--${OPTARG}', value: '${val}'" >&2;
-                    tagversion="$val"
-                    ;;
-                tag=*)
-                    val=${OPTARG#*=}
-                    opt=${OPTARG%=$val}
-                    echo "Parsing option: '--${opt}', value: '${val}'" >&2
-                    tagversion="$val"
-                    ;;
-                help)
-                    hilfe ''
-                    exit
-                    ;;
-                *)
-                    if [ "$OPTERR" = 1 ] && [ "${optspec:0:1}" != ":" ]; then
-                        hilfe "Unknown option --${OPTARG}"
-                        exit
-                    fi
-                    ;;
-            esac;;
-        # Evaluate short options
-        b)
-            val="${!OPTIND}";
-            echo "Parsing option: '-${optchar}'" >&2
-            build=true
-            ;;
-        h)
-            val="${!OPTIND}";
-            hilfe ''
-            exit 0
-            ;;
-        *)
-            if [ "$OPTERR" != 1 ] || [ "${optspec:0:1}" = ":" ]; then
-                echo "Non-option argument: '-${OPTARG}'" >&2
-            fi
-            ;;
-    esac
+echo "# evaluate params"
+die() { echo "$*" >&2; exit 2; }  # complain to STDERR and exit with error
+needs_arg() { if [ -z "$OPTARG" ]; then die "No arg for --$OPT option"; fi; }
+optspec=":vhb:t:-:" # allow -b with arg, -t with arg, -v, -h, and -- "with arg"
+while getopts $optspec OPT; do
+  # support long options: https://stackoverflow.com/a/28466267/519360
+  if [ "$OPT" = "-" ]; then   # long option: reformulate OPT and OPTARG
+    OPT="${OPTARG%%=*}"       # extract long option name
+    OPTARG="${OPTARG#"$OPT"}" # extract long option argument (may be empty)
+    OPTARG="${OPTARG#=}"      # if long option argument, remove assigning `=`
+  fi
+  case "$OPT" in
+    b | build )    needs_arg; build="$OPTARG" ;;
+    t | tag   )    needs_arg; tagversion="$OPTARG" ;;
+    h | help )     hilfe '';exit ;;  # optional argument
+    \? )           hilfe 'bad short option'; exit 2 ;;      # bad short option (error reported via getopts)
+    * )            hilfe "Illegal option --$OPT"; exit 2 ;; # bad long option
+  esac
 done
+shift $((OPTIND-1))    # remove parsed options and args from $@ list
 
+# ======================  M A I N  ======================
+
+if [ "$tagversion" == 'newest' ]; then
+    tagversion="$(get-git-tag.sh)"
+    prompt -p "# Do you want to checkout the tag $tagversion? [y/N]" answer
+    if [[ ! $answer =~ ^[Yy] ]]; then
+        tagversion='latest'
+        echo "# Then i build with git tag 'latest'"
+    fi
+fi
+echo "# remember cluster"
 remember-current-cluster
+
 # login in the build cluster, which is cid-scp0, where the scp-build namespace is located
 if ocl cid-scp0 -d > /dev/null 2>&1 &&
     ocl > /dev/null 2>&1; then  # login in the current cluster
-    echo "working on CLUSTER: $CLUSTER"
+    echo "# working on CLUSTER: $CLUSTER"
 else
     echo "ERROR: ocl login failed" 1>&2
     exit 1
 fi
 
 echo "# B U I L D   L O C A L   A N D   D E P L O Y   T O   A R T I F A C T O R Y"
-if [ "$build" == 'true' ]; then
+if [ "$build" != 'false' ]; then
     if [ "$tagversion" != 'latest' ]; then
         # Ensure git checkout master is executed on script exit
         trap 'git checkout master' EXIT
-        echo "git checkout $tagversion"
+        echo "# git checkout $tagversion"
         git checkout "$tagversion"
     fi
-    echo "Build kpasscli local and deploy to artifactory"
-    "$scriptdir"/_build-and-deploy-to-artifactory.sh
-fi
 
+    # build the $build and deploy to artifactory
+    echo "# Build $build local and deploy to artifactory"
+    if "$scriptdir"/_build-and-deploy-to-artifactory.sh $build; then
+        echo "# Build and deploy to artifactory was successful"
+    else
+        echo "ERROR: Build and deploy to artifactory failed" 1>&2
+    fi
+
+    # build the $build image and deploy to artifactory
+    if [ "$build_service_image" != 'false' ]; then
+        echo "###### B u i l d    i m a g e   $build_service_image   w i t h   $tagversion   ########"
+        echo "$scriptdir"/_build-and-deploy-image.sh $build_service_image $tagversion
+        if "$scriptdir"/_build-and-deploy-image.sh $build_service_image $tagversion; then
+            echo "# Build and deploy service, deploy image to quay was successful"
+        else
+            echo "ERROR: Build and deploy service, deploy image to quay was failed" 1>&2
+        fi
+    fi
+
+fi
 
 switch-back-to-current-cluster
