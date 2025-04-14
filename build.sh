4ccacddf26c1e693a0e842ce56a12a5597f9f1cd
Dirk Jäger <dirk.jaeger.dj@gmail.com>
2025-04-14 15:28:45 +0200

    refactor

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# echo Generate the config-clusters.go
+# build/scripts/generate_config.sh
+
+echo "Build linux binary of kpasscli"
+mkdir -p dist/linux-amd64
+GOOS=linux GOARCH=amd64 go build -v -o dist/linux-amd64/kpasscli
+
+echo "Build windows binary of kpasscli"
+mkdir -p dist/windows-amd64
+GOOS=windows GOARCH=amd64 go build -v -o dist/windows-amd64/kpasscli.exe
+
+echo "Build darwin binary of kpasscli"
+mkdir -p dist/darwin-amd64
+GOOS=darwin GOARCH=amd64 go build -v -o dist/darwin-amd64/kpasscli
+
+echo "Build darwin arm64 binary of kpasscli"
+mkdir -p dist/darwin-arm64
+GOOS=darwin GOARCH=arm64 go build -v -o dist/darwin-arm64/kpasscli
--- src/keepass/keepass.go
+++ src/keepass/keepass.go
@@ -10,7 +10,7 @@ import (
 	"syscall"
 
 	"github.com/tobischo/gokeepasslib/v3"
-	"golang.org/x/crypto/ssh/terminal"
+	"golang.org/x/term"
 )
 
 // OpenDatabase opens and decodes a KeePass database file.
@@ -78,11 +78,12 @@ func ResolvePassword(passParam string, cfg *config.Config, kdbpassenv string) (s
 		return getPasswordFromPrompt()
 	}
 	// Resolve environment variables in passfile
-	passfile = os.ExpandEnv(passfile)
-	debug.Log(passfile)
+	// passfile = os.ExpandEnv(passfile)
+	debug.Log("PassFile: %v", passfile)
 
 	// Check if passfile is an executable in $PATH
 	if execPath, err := exec.LookPath(passfile); err == nil {
+		debug.Log("passfile: %v is an executable: %v", passfile, execPath)
 		passfile = execPath
 	}
 	info, err := os.Stat(passfile)
@@ -90,7 +91,7 @@ func ResolvePassword(passParam string, cfg *config.Config, kdbpassenv string) (s
 		debug.Log("passfile: %v Error: %v", passfile, err.Error())
 		return "", fmt.Errorf("password must be provided via file or executable")
 	}
-	debug.Log("%v", info)
+	debug.Log("%+v", info)
 
 	if info.Mode()&os.ModeNamedPipe != 0 {
 		// Read password from process substitution
@@ -141,7 +142,7 @@ func getPasswordFromPrompt() (string, error) {
 	// If no valid file or executable is found, prompt the user for the password
 	fmt.Print("Enter password: ")
 	var password string
-	passwordBytes, err := terminal.ReadPassword(int(syscall.Stdin))
+	passwordBytes, err := term.ReadPassword(int(syscall.Stdin))
 	if err != nil {
 	}
 	password = strings.TrimSpace(string(passwordBytes))
